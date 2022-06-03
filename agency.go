b29d063d880e0824947d09e34afc0e9eca525ad2
Jefferson Neves <jefferson.rpn@gmail.com>
2022-06-03 11:10:39 -0300

    Ajusta erro de digitação

--- agency.go
+++ agency.go
@@ -28,7 +28,7 @@ type Agency struct {
 	Entity     string       `json:"entity" bson:"entity,omitempty"` // "J" For Judiciário, "M" for Ministério Público, "P" for Procuradorias and "D" for Defensorias.
 	UF         string       `json:"uf" bson:"uf,omitempty"`         // Short code for federative unity.
 	FlagURL    string       `json:"url" bson:"url,omitempty"`       // Link for state url
-	Collecting []Collecting `json:"colleting" bson:"colleting,omitempty"`
+	Collecting []Collecting `json:"collecting" bson:"collecting,omitempty"`
 }
 
 // Collecting A Struct containing the day we checked the status of the data and the reasons why we didn't collected it.
