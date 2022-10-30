6db9f651c474495f7c0b3689470ed0c5c1e5bfe5
duardoqueiroz <eduardoldq1@gmail.com>
2022-10-30 20:35:40 -0300

    chore: change models

--- agency.go
+++ agency.go
@@ -16,96 +16,101 @@
 package storage
 
 import (
+	"time"
+
 	"github.com/dadosjusbr/proto/coleta"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// Agency A Struct containing the main descriptions of each Agency.
-type Agency struct {
-	ID         string       `json:"aid" bson:"aid,omitempty"`       // 'trt13'
-	Name       string       `json:"name" bson:"name,omitempty"`     // 'Tribunal Regional do Trabalho 13° Região'
-	Type       string       `json:"type" bson:"type,omitempty"`     // "R" for Regional, "M" for Municipal, "F" for Federal, "E" for State.
-	Entity     string       `json:"entity" bson:"entity,omitempty"` // "J" For Judiciário, "M" for Ministério Público, "P" for Procuradorias and "D" for Defensorias.
-	UF         string       `json:"uf" bson:"uf,omitempty"`         // Short code for federative unity.
-	FlagURL    string       `json:"url" bson:"url,omitempty"`       // Link for state url
-	Collecting []Collecting `json:"collecting" bson:"collecting,omitempty"`
+// Estrutura que contém as principais descrições de cada órgão.
+type Orgao struct { 
+	ID         string      `json:"id" bson:"id,omitempty" gorm:"column:id"`                 // 'trt13'
+	Nome       string      `json:"nome" bson:"nome,omitempty" gorm:"column:nome"`             // 'Tribunal Regional do Trabalho 13° Região'
+	Jurisdicao string      `json:"jurisdicao" bson:"jurisdicao,omitempty" gorm:"column:jurisdicao"` // Estadual, Trabalho, Federal
+	Entidade   string      `json:"entidade" bson:"entidade,omitempty" gorm:"column:entidade"`     // Ministério, Tribunal
+	UF         string      `json:"uf" bson:"uf,omitempty" gorm:"column:uf"`                 // Unidade Federativa
+	FlagURL    string      `json:"url" bson:"url,omitempty"`               // Link for state url
+	Coletando  []Coletando `json:"coletando" bson:"coletando,omitempty"`
 }
 
-// Collecting A Struct containing the day we checked the status of the data and the reasons why we didn't collected it.
-type Collecting struct {
-	Timestamp   *timestamppb.Timestamp `json:"timestamp" bson:"timestamp,omitempty"`     // Day(unix) we checked the status of the data
-	Description []string               `json:"description" bson:"description,omitempty"` // Reasons why we didn't collect the data
+// Estrutura que contém o dia em que verificamos o status dos dados e as razões pelas quais não os coletamos.
+type Coletando struct {
+	Timestamp *timestamppb.Timestamp `json:"timestamp" bson:"timestamp,omitempty"` // Day(unix) we checked the status of the data
+	Descricao []string               `json:"descricao" bson:"descricao,omitempty"` // Reasons why we didn't collect the data
 }
 
-// AgencyMonthlyInfo A Struct containing a snapshot of a agency in a month.
-
-type AgencyMonthlyInfo struct {
-	AgencyID          string                 `json:"aid,omitempty" bson:"aid,omitempty"`
-	Month             int                    `json:"month,omitempty" bson:"month,omitempty"`
-	Year              int                    `json:"year,omitempty" bson:"year,omitempty"`
-	Backups           []Backup               `json:"backups,omitempty" bson:"backups,omitempty"`
-	Summary           Summary                `json:"summary,omitempty" bson:"summary,omitempty"`
-	CrawlerID         string                 `json:"crawler_id,omitempty" bson:"crawler_id,omitempty"`
-	CrawlerVersion    string                 `json:"crawler_version,omitempty" bson:"crawler_version,omitempty"`
-	CrawlerDir        string                 `json:"crawler_dir,omitempty" bson:"crawler_dir,omitempty"`
-	CrawlerRepo       string                 `json:"crawler_repo,omitempty" bson:"crawler_repo,omitempty"` // The github Repository of MI Crawler
-	CrawlingTimestamp *timestamppb.Timestamp `json:"crawling_ts,omitempty" bson:"crawling_ts,omitempty"`   // Crawling moment (always UTC)
-	ProcInfo          *coleta.ProcInfo       `json:"procinfo,omitempty" bson:"procinfo,omitempty"`         // Making this a pointer because it should be an optional field due to backwards compatibility.
-	Package           *Backup                `json:"package,omitempty" bson:"package,omitempty"`           // Making this a pointer because it should be an optional field due to backwards compatibility.
-	Meta              *Meta                  `json:"meta,omitempty" bson:"meta,omitempy"`
-	Score             *Score                 `json:"score,omitempty" bson:"score,omitempy"`
-	ExectionTime      float64                `json:"exection_time,omitempty" bson:"exection_time,omitempty"`
+// Estrutura que contém as informações de uma coleta.
+type Coleta struct {
+	ID                 string           `json:"id" gorm:"column:id"` // 'trt13/01/2020'
+	Timestamp          time.Time        `json:"timestamp,omitempty" bson:"timestamp,omitempty" gorm:"column:timestamp"`
+	IdOrgao            string           `json:"id_orgao,omitempty" bson:"aid,omitempty" gorm:"column:id_orgao"`
+	Mes                int              `json:"mes,omitempty" bson:"month,omitempty" gorm:"column:mes"`
+	Ano                int              `json:"ano,omitempty" bson:"year,omitempty" gorm:"column:ano"`
+	Atual              bool             `json:"atual,omitempty" gorm:"column:atual"`
+	Backup             []Backup         `json:"backups,omitempty" bson:"backups,omitempty" gorm:"column:backups"`
+	Sumario            Sumario         	`json:"sumario,omitempty" bson:"summary,omitempty" gorm:"column:sumario"`
+	RepositorioColetor string           `json:"repositorio_coletor,omitempty" bson:"crawler_repo,omitempty" gorm:"column:repositorio_coletor"`
+	VersaoColetor      string           `json:"versao_coletor,omitempty" bson:"crawler_version,omitempty" gorm:"column:versao_coletor"`
+	RepositorioParser  string           `json:"repositorio_parser,omitempty" bson:"parser_repository,omitempty" gorm:"column:repositorio_parser"`
+	VersaoParser       string           `json:"versao_parser,omitempty" bson:"parser_version,omitempty" gorm:"column:versao_parser"`
+	ProcInfo           *coleta.ProcInfo `json:"procinfo,omitempty" bson:"procinfo,omitempty" gorm:"procinfo"`
+	Package            *Backup          `json:"package,omitempty" bson:"package,omitempty" gorm:"package"`
+	Meta               *Meta       			`json:"meta,omitempty" bson:"metadata,omitempty"`
+	Indice             *Indice          `json:"indices,omitempty" bson:"score,omitempty"`
 }
 
 // Backup contains the URL to download a file and a hash to track if in the future will be changes in the file.
 type Backup struct {
 	URL  string `json:"url" bson:"url,omitempty"`
 	Hash string `json:"hash" bson:"hash,omitempty"`
+	Size int64  `json:"size" bson:"size,omitempty"`
 }
 
 // Summaries contains all summary detailed information
 type Summaries struct {
-	General       Summary `json:"general,omitempty" bson:"general"`
-	MemberActive  Summary `json:"memberactive,omitempty" bson:"memberactive"`
-	Undefined     Summary `json:"undefined,omitempty" bson:"undefined"`
-	ServantActive Summary `json:"servantactive,omitempty" bson:"servantactive"`
+	General       Sumario `json:"general,omitempty" bson:"general"`
+	MemberActive  Sumario `json:"memberactive,omitempty" bson:"memberactive"`
+	Undefined     Sumario `json:"undefined,omitempty" bson:"undefined"`
+	ServantActive Sumario `json:"servantactive,omitempty" bson:"servantactive"`
 }
 
-// Summary A Struct containing summarized  information about a agency/month stats
-type Summary struct {
-	Count              int         `json:"count" bson:"count,omitempty"`                             // Number of employees
-	BaseRemuneration   DataSummary `json:"base_remuneration" bson:"base_remuneration,omitempty"`     //  Statistics (Max, Min, Median, Total)
-	OtherRemunerations DataSummary `json:"other_remunerations" bson:"other_remunerations,omitempty"` //  Statistics (Max, Min, Median, Total)
-	IncomeHistogram    map[int]int `json:"hist" bson:"hist,omitempty"`
+// O sumário contém estatisitcas sobre a coleta de um mês de um órgão.
+type Sumario struct {
+	Membros            int         `json:"membros,omitempty" bson:"count,omitempty"`
+	RemuneracaoBase    DataSummary `json:"remuneracao_base,omitempty" bson:"base_remuneration,omitempty"`
+	OutrasRemuneracoes DataSummary `json:"outras_remuneracoes,omitempty" bson:"other_remunerations,omitempty"`
+	HistogramaRenda    map[int]int `json:"histograma_renda,omitempty" bson:"hist,omitempty"`
 }
 
 // DataSummary A Struct containing data summary with statistics.
 type DataSummary struct {
-	Max     float64 `json:"max" bson:"max,omitempty"`
+	Max     float64 `json:"max" bson:"max,omitempty"` 
 	Min     float64 `json:"min" bson:"min,omitempty"`
 	Average float64 `json:"avg" bson:"avg,omitempty"`
 	Total   float64 `json:"total" bson:"total,omitempty"`
 }
 
+// Estrutura que contém os metadados de uma coleta.
 type Meta struct {
-	NoLoginRequired   bool   `json:"no_login_required,omitempty" bson:"no_login_required,omitempty"`
-	NoCaptchaRequired bool   `json:"no_captcha_required,omitempty" bson:"no_captcha_required,omitempty"`
-	Access            string `json:"access,omitempty" bson:"access,omitempty"`
-	Extension         string `json:"extension,omitempty" bson:"extension,omitempty"`
-	StrictlyTabular   bool   `json:"strictly_tabular,omitempty" bson:"strictly_tabular,omitempty"`
-	ConsistentFormat  bool   `json:"consistent_format,omitempty" bson:"consistent_format,omitempty"`
-	HaveEnrollment    bool   `json:"have_enrollment,omitempty" bson:"have_enrollment,omitempty"`
-	ThereIsACapacity  bool   `json:"there_is_a_capacity,omitempty" bson:"there_is_a_capacity,omitempty"`
-	HasPosition       bool   `json:"has_position,omitempty" bson:"has_position,omitempty"`
-	BaseRevenue       string `json:"base_revenue,omitempty" bson:"base_revenue,omitempty"`
-	OtherRecipes      string `json:"other_recipes,omitempty" bson:"other_recipes,omitempty"`
-	Expenditure       string `json:"expenditure,omitempty" bson:"expenditure,omitempty"`
+	NaoRequerLogin             bool   `json:"no_login_required,omitempty" bson:"no_login_required,omitempty" gorm:"column:nao_requer_login"`
+	NaoRequerCaptcha           bool   `json:"no_captcha_required,omitempty" bson:"no_captcha_required,omitempty" gorm:"column:nao_requer_captcha"`
+	Acesso                     string `json:"access,omitempty" bson:"access,omitempty" gorm:"column:acesso"`
+	Extensao                   string `json:"extension,omitempty" bson:"extension,omitempty" gorm:"column:extensao"`
+	EstritamenteTabular        bool   `json:"strictly_tabular,omitempty" bson:"strictly_tabular,omitempty" gorm:"column:estritamente_tabular"`
+	FormatoConsistente         bool   `json:"consistent_format,omitempty" bson:"consistent_format,omitempty" gorm:"column:formato_consistente"`
+	TemMatricula               bool   `json:"have_enrollment,omitempty" bson:"have_enrollment,omitempty" gorm:"column:tem_matricula"`
+	TemLotacao                 bool   `json:"there_is_a_capacity,omitempty" bson:"there_is_a_capacity,omitempty" gorm:"column:tem_lotacao"`
+	TemCargo                   bool   `json:"has_position,omitempty" bson:"has_position,omitempty" gorm:"column:tem_cargo"`
+	DetalhamentoReceitaBase    string `json:"detalhamento_receita_base,omitempty" bson:"base_revenue,omitempty" gorm:"column:detalhamento_receita_base"`
+	DetalhamentoOutrasReceitas string `json:"detalhamento_outras_receitas,omitempty" bson:"other_recipes,omitempty" gorm:"column:detalhamento_outras_receitas"`
+	DetalhamentoDescontos      string `json:"detalhamento_descontos,omitempty" bson:"expenditure,omitempty" gorm:"column:detalhamento_descontos"`
 }
 
-type Score struct {
-	Score             float64 `json:"score,omitempty" bson:"score,omitempty"`
-	CompletenessScore float64 `json:"completeness_score,omitempty" bson:"completeness_score,omitempty"`
-	EasinessScore     float64 `json:"easiness_score,omitempty" bson:"easiness_score,omitempty"`
+// Indices de uma coleta.
+type Indice struct {
+	IndiceTransparencia float64 `json:"indice_transparencia,omitempty" bson:"score,omitempty" gorm:"column:indice_transparencia"`
+	IndiceCompletude    float64 `json:"indice_completude,omitempty" bson:"completeness_score,omitempty" gorm:"column:indice_completude"`
+	IndiceFacilidade    float64 `json:"indice_facilidade,omitempty" bson:"easiness_score,omitempty" gorm:"column:indice_facilidade"`
 }
 
 // MonthlyInfoVersion é um item do histórico de coletas armazenado no banco de dados.
@@ -114,5 +119,5 @@ type MonthlyInfoVersion struct {
 	Month     int               `json:"month,omitempty" bson:"month,omitempty"`
 	Year      int               `json:"year,omitempty" bson:"year,omitempty"`
 	VersionID int64             `json:"version_id,omitempty" bson:"version_id,omitempty"` // revisão/versão do irem. O tipo é int64 pois podemos querer usar epoch para ficar mais simples.
-	Version   AgencyMonthlyInfo `json:"version,omitempty" bson:"version,omitempty"`
+	Version   Coleta `json:"version,omitempty" bson:"version,omitempty"`
 }
