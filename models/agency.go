2fba2f8d2ba6a2ac9e91ff59650e38ce7a38e1a9
duardoqueiroz <eduardoldq1@gmail.com>
2022-11-22 10:33:07 -0300

    fix: format file

--- models/agency.go
+++ models/agency.go
@@ -2,12 +2,12 @@ package models
 
 // Agency A Struct containing the main descriptions of each Agency.
 type Agency struct {
-	ID         string       `json:"aid" bson:"aid,omitempty"`             // 'trt13'
-	Name       string       `json:"name" bson:"name,omitempty"`         // 'Tribunal Regional do Trabalho 13° Região'
-	Type       string       `json:"type" bson:"type,omitempty"`   // "R" for Regional, "M" for Municipal, "F" for Federal, "E" for State.
-	Entity     string       `json:"entity" bson:"entity,omitempty"` // "J" For Judiciário, "M" for Ministério Público, "P" for Procuradorias and "D" for Defensorias.
-	UF         string       `json:"uf" bson:"uf,omitempty"`               // Short code for federative unity.
-	FlagURL    string       `json:"url" bson:"url,omitempty"`                              // Link for state url
+	ID         string       `json:"aid" bson:"aid,omitempty"`				// 'trt13'
+	Name       string       `json:"name" bson:"name,omitempty"`			// 'Tribunal Regional do Trabalho 13° Região'
+	Type       string       `json:"type" bson:"type,omitempty"`			// "R" for Regional, "M" for Municipal, "F" for Federal, "E" for State.
+	Entity     string       `json:"entity" bson:"entity,omitempty"`	// "J" For Judiciário, "M" for Ministério Público, "P" for Procuradorias and "D" for Defensorias.
+	UF         string       `json:"uf" bson:"uf,omitempty"`					// Short code for federative unity.
+	FlagURL    string       `json:"url" bson:"url,omitempty"`				// Link for state url
 	Collecting []Collecting `json:"collecting" bson:"collecting,omitempty"`
 }
 
