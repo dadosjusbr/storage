a7c8d66f36b4049f05bfa9e280c2053c633f450e
duardoqueiroz <eduardoldq1@gmail.com>
2022-12-12 11:48:18 -0300

    fix: dto

--- repositories/database/postgres/postgres.go
+++ repositories/database/postgres/postgres.go
@@ -279,8 +279,9 @@ func (p *PostgresDB) GetFirstDateWithMonthlyInfo() (int, int, error) {
 }
 
 func (p *PostgresDB) GetLastDateWithMonthlyInfo() (int, int, error) {
+	var dtoAgmi dto.AgencyMonthlyInfoDTO
 	var year, month int
-	m := p.db.Table("coletas").Select("MAX(ano),MAX(mes)")
+	m := p.db.Model(&dtoAgmi).Select("MAX(ano),MAX(mes)")
 	m = m.Where("atual=true AND (procinfo IS NULL OR procinfo::text='null')")
 	if err := m.Row().Scan(&year, &month); err != nil {
 		return 0, 0, fmt.Errorf("error getting last date with monthly info: %q", err)
