a6504786ab763ab0c7396a8b282415319f8f1255
duardoqueiroz <eduardoldq1@gmail.com>
2022-12-20 10:11:34 -0300

    feat: get general monthly info from year

---
+++ repositories/database/postgres/dto/generalMonthlyInfo.go
@@ -0,0 +1,30 @@
+package dto
+
+import (
+	"github.com/dadosjusbr/storage/models"
+)
+
+type GeneralMonthlyInfoDTO struct {
+	Month              int     `gorm:"column:mes"`
+	Count              int     `gorm:"column:num_membros"`
+	BaseRemuneration   float64 `gorm:"column:remuneracao_base"`
+	OtherRemunerations float64 `gorm:"column:outras_remuneracoes"`
+}
+
+func NewGeneralMonthlyInfoDTO(gmi models.GeneralMonthlyInfo) *GeneralMonthlyInfoDTO {
+	return &GeneralMonthlyInfoDTO{
+		Month:              gmi.Month,
+		Count:              gmi.Count,
+		BaseRemuneration:   gmi.BaseRemuneration,
+		OtherRemunerations: gmi.OtherRemunerations,
+	}
+}
+
+func (gmi *GeneralMonthlyInfoDTO) ConvertToModel() *models.GeneralMonthlyInfo {
+	return &models.GeneralMonthlyInfo{
+		Month:              gmi.Month,
+		Count:              gmi.Count,
+		BaseRemuneration:   gmi.BaseRemuneration,
+		OtherRemunerations: gmi.OtherRemunerations,
+	}
+}
