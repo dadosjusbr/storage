6d44960b36d2b0e87e019056da562f3fa8552704
Joellensilva <joellen.absilva@gmail.com>
2024-06-11 17:12:19 -0300

    adicionando coluna nome_sanitizado

--- repo/database/dto/paychecksDTO.go
+++ repo/database/dto/paychecksDTO.go
@@ -3,20 +3,21 @@ package dto
 import "github.com/dadosjusbr/storage/models"
 
 type PaycheckDTO struct {
-	ID           int     `gorm:"column:id"`
-	Agency       string  `gorm:"column:orgao"`
-	Month        int     `gorm:"column:mes"`
-	Year         int     `gorm:"column:ano"`
-	CollectKey   string  `gorm:"column:chave_coleta"`
-	Name         string  `gorm:"column:nome"`
-	RegisterID   string  `gorm:"column:matricula"`
-	Role         string  `gorm:"column:funcao"`
-	Workplace    string  `gorm:"column:local_trabalho"`
-	Salary       float64 `gorm:"column:salario"`
-	Benefits     float64 `gorm:"column:beneficios"`
-	Discounts    float64 `gorm:"column:descontos"`
-	Remuneration float64 `gorm:"column:remuneracao"`
-	Situation    *string `gorm:"column:situacao"`
+	ID            int     `gorm:"column:id"`
+	Agency        string  `gorm:"column:orgao"`
+	Month         int     `gorm:"column:mes"`
+	Year          int     `gorm:"column:ano"`
+	CollectKey    string  `gorm:"column:chave_coleta"`
+	Name          string  `gorm:"column:nome"`
+	RegisterID    string  `gorm:"column:matricula"`
+	Role          string  `gorm:"column:funcao"`
+	Workplace     string  `gorm:"column:local_trabalho"`
+	Salary        float64 `gorm:"column:salario"`
+	Benefits      float64 `gorm:"column:beneficios"`
+	Discounts     float64 `gorm:"column:descontos"`
+	Remuneration  float64 `gorm:"column:remuneracao"`
+	Situation     *string `gorm:"column:situacao"`
+	SanitizedName string  `gorm:"column:nome_sanitizado"`
 }
 
 func (PaycheckDTO) TableName() string {
@@ -25,38 +26,40 @@ func (PaycheckDTO) TableName() string {
 
 func (p PaycheckDTO) ConvertToModel() *models.Paycheck {
 	return &models.Paycheck{
-		ID:           p.ID,
-		Agency:       p.Agency,
-		Month:        p.Month,
-		Year:         p.Year,
-		CollectKey:   p.CollectKey,
-		Name:         p.Name,
-		RegisterID:   p.RegisterID,
-		Role:         p.Role,
-		Workplace:    p.Workplace,
-		Salary:       p.Salary,
-		Benefits:     p.Benefits,
-		Discounts:    p.Discounts,
-		Remuneration: p.Remuneration,
-		Situation:    p.Situation,
+		ID:            p.ID,
+		Agency:        p.Agency,
+		Month:         p.Month,
+		Year:          p.Year,
+		CollectKey:    p.CollectKey,
+		Name:          p.Name,
+		RegisterID:    p.RegisterID,
+		Role:          p.Role,
+		Workplace:     p.Workplace,
+		Salary:        p.Salary,
+		Benefits:      p.Benefits,
+		Discounts:     p.Discounts,
+		Remuneration:  p.Remuneration,
+		Situation:     p.Situation,
+		SanitizedName: p.SanitizedName,
 	}
 }
 
 func NewPaycheckDTO(p models.Paycheck) *PaycheckDTO {
 	return &PaycheckDTO{
-		ID:           p.ID,
-		Agency:       p.Agency,
-		Month:        p.Month,
-		Year:         p.Year,
-		CollectKey:   p.CollectKey,
-		Name:         p.Name,
-		RegisterID:   p.RegisterID,
-		Role:         p.Role,
-		Workplace:    p.Workplace,
-		Salary:       p.Salary,
-		Benefits:     p.Benefits,
-		Discounts:    p.Discounts,
-		Remuneration: p.Remuneration,
-		Situation:    p.Situation,
+		ID:            p.ID,
+		Agency:        p.Agency,
+		Month:         p.Month,
+		Year:          p.Year,
+		CollectKey:    p.CollectKey,
+		Name:          p.Name,
+		RegisterID:    p.RegisterID,
+		Role:          p.Role,
+		Workplace:     p.Workplace,
+		Salary:        p.Salary,
+		Benefits:      p.Benefits,
+		Discounts:     p.Discounts,
+		Remuneration:  p.Remuneration,
+		Situation:     p.Situation,
+		SanitizedName: p.SanitizedName,
 	}
 }
